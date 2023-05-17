61f6d24ebc5ab4b26b6ce2900b503a8011d07101
Emmanuel <oyedelee22@gmail.com>
2023-05-17 08:57:24 +0100

    added fetch quote

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+
+import {useQuery} from 'react-query';  
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const { isLoading, refetch, data, error} = useQuery ('quoteData', fetchQuote)
+
+
+    console.log(data)
+      
+
+    async function fetchQuote (){
+      const res =  await fetch('https://api.quotable.io/random'); 
+      if(!res.ok){ 
+        throw error ('Failed to get quote')
+      }
+      return res.json();
+
+    }
+
+
+    const getNewQuote = (e) =>{ 
+      e.preventDefault()
+
+    refetch()
+
+    }
+
+      if(isLoading){ 
+        return  <div>
+          Loading
+        </div>
+      }
+      if(error){
+        return <div>Error: {error.message}</div>
+      }
+
+
+return (
+  <>
+  
+  <div className='container'>
+
+      <div className='quote-wrap'> 
+        <div className='quote-author--center'> 
+          <p className='quote-word'> {data.content} </p>
+          <small className='quote-author'>
+              {data.author}
+          </small>
+        </div>
+        <button className='btn' onClick={getNewQuote}>New Quote</button>
+      </div>
+
+      <small className='footer'> 
+          <a href='github.com/cjwalt'>cjwalt</a>
+      </small>
+
+</div>
+
+
+
+  </>
+     
   );
+
 }
 
 export default App;
