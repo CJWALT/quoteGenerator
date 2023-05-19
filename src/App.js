d74f69de498a421a87ec5298797c67c3c358cdbc
Emmanuel <oyedelee22@gmail.com>
2023-05-19 17:05:24 +0100

    added dark-light mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
+import { useState } from 'react';
 import {useQuery} from 'react-query';  
 import './App.css';
 
 
 function App() {
 
+  const [lightDark, setLightDark] = useState(true)
+ 
 const { isLoading, refetch, data, error} = useQuery ('quoteData', fetchQuote)
 
 
@@ -26,10 +29,21 @@ const { isLoading, refetch, data, error} = useQuery ('quoteData', fetchQuote)
       refetch()
 
     }
+    
+    const toggleDark = () =>{ 
+        setLightDark (!lightDark)
+    }
+
+    if(!lightDark){ 
+      document.body.classList.add('body-dark');
+    }
+    else{ 
+      document.body.classList.remove('body-dark');
+    }
 
       if(isLoading){ 
         return <div className='loading'>
-            <p>Loading</p>
+            <p>Loading...</p>
         </div>
         
       }
@@ -42,20 +56,23 @@ return (
   <>
   
   <div className='container'>
-
+    <div className='light-dark'> 
+      
+      {lightDark ? <ion-icon name="sunny-outline" className="light-icon" onClick={toggleDark}></ion-icon> :  <ion-icon name="contrast-outline" onClick={toggleDark} ></ion-icon> }
+    </div>
       <div className='quote-wrap'> 
-          <h4 className='quote-heading'>click to see random inspiration/love quote</h4>
+          <h4 className={lightDark ? 'quote-heading': 'quote-heading dark-txt'}>click to see random inspiration/love quote</h4>
         <div className='quote-author--center'> 
-          <p className='quote-word'> {data.content} </p>
-          <small className='quote-author'>
+          <p className={lightDark ? 'quote-word' : 'quote-word dark-txt'}> {data.content} </p>
+          <small className={lightDark ? 'quote-author' : 'quote-author author-txt'}>
               -{data.author}
           </small>
         </div>
-        <button className='btn' onClick={getNewQuote}>New Quote</button>
+        <button className={lightDark ? 'btn' : 'btn dark-btn'} onClick={getNewQuote}>New Quote</button>
       
 
       <small className='footer'> 
-          <a href='https://www.github.com/cjwalt'>cjwalt</a>
+          <a href='https://www.github.com/cjwalt' className={lightDark ? undefined : 'dark-txt' }>cjwalt</a>
       </small>
       </div>
 
