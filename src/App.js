1ae70d5f2c2bcc5e1aab9a7d1a4f2521457d1c99
Emmanuel <oyedelee22@gmail.com>
2023-05-19 15:34:00 +0100

    new updates

--- src/App.js
+++ src/App.js
@@ -23,14 +23,15 @@ const { isLoading, refetch, data, error} = useQuery ('quoteData', fetchQuote)
     const getNewQuote = (e) =>{ 
       e.preventDefault()
 
-    refetch()
+      refetch()
 
     }
 
       if(isLoading){ 
-        return  <div>
-          Loading
+        return <div className='loading'>
+            <p>Loading</p>
         </div>
+        
       }
       if(error){
         return <div>Error: {error.message}</div>
@@ -43,6 +44,7 @@ return (
   <div className='container'>
 
       <div className='quote-wrap'> 
+          <h4 className='quote-heading'>click to see random inspiration/love quote</h4>
         <div className='quote-author--center'> 
           <p className='quote-word'> {data.content} </p>
           <small className='quote-author'>
@@ -50,11 +52,12 @@ return (
           </small>
         </div>
         <button className='btn' onClick={getNewQuote}>New Quote</button>
-      </div>
+      
 
       <small className='footer'> 
-          <a href='github.com/cjwalt'>cjwalt</a>
+          <a href='https://www.github.com/cjwalt'>cjwalt</a>
       </small>
+      </div>
 
 </div>
 
